4bfd7fd8f2e286af19f48d7d866fb49124db8d9c
jabir-msf <jabir1472007@gmail.com>
2026-07-09 20:59:14 +0600

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN,
-    CHANNEL_USERNAME: process.env.CHANNEL_USERNAME,
+    CHANNEL_ID: process.env.CHANNEL_ID, // ইউজারনেমের বদলে আইডি ব্যবহার করছি
     WEBAPP_URL: process.env.WEBAPP_URL,
     PORT: process.env.PORT || 3000
 };
