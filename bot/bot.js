adb338fb9248cb8a8359ca78d75393f1805ee1df
jabir-msf <jabir1472007@gmail.com>
2026-07-09 16:17:05 +0600

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -3,6 +3,7 @@ const { BOT_TOKEN } = require("./config");
 
 // Handlers
 const startHandler = require("./handlers/start");
+const verifyHandler = require("./handlers/verify");
 
 if (!BOT_TOKEN) {
     throw new Error("BOT_TOKEN is missing. Add it to your .env file.");
@@ -14,5 +15,6 @@ const bot = new TelegramBot(BOT_TOKEN, {
 
 // Register Handlers
 startHandler(bot);
+verifyHandler(bot);
 
 console.log("🤖 EarnBD Pro Bot Started Successfully!");
